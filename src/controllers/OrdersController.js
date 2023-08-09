c780a3cb5af16185265ea077fe2aedca6944b28e
Larissa <lara.adler@hotmail.com>
2023-08-09 12:01:58 -0300

    Atualização do OrdrsController e sua rota.

--- src/controllers/OrdersController.js
+++ src/controllers/OrdersController.js
@@ -1,25 +1,118 @@
 const AppError = require("../utils/AppError");
+
 const knex = require("../database/knex");
 
+const moment = require("moment-timezone");
 class OrdersController {
-  async create(request, response) { 
-    const { status } = request.body;
-    const { user_id} = request.params;
+    async create(request, response) {
+        const user_id = request.user.id;
 
-    const order = await knex("orders").where({ email }).first(); //'first' para pegar apenas um email, e trazer os dados do usuário que possui esse email filtrado
+        const saoPauloTime = moment.tz(Date.now(), 'America/Sao_Paulo');
 
-    if(!user) { //se usuário não existir, valor Falso
-      throw new AppError("E-mail e/ou senha incorreta", 401); //jogando uma exceção e tratando ela
+        const formattedDate = saoPauloTime.format('DD/MM - HH:mm');
+        
+        if(!user_id) {
+            throw new AppError("Se conecte para poder comprar.")
+        }
+
+        const [id] = await knex("orders").insert({ 
+            user_id,
+            created_at: formattedDate,
+            updated_at: formattedDate
+        });
+        
+        return response.json({
+            id
+        });
     }
 
-    const [ der ] = await knex("order").insert({
-     user_id,
-     status
-    });
+    async show(request, response) {
+        const user_id = request.user.id;
 
-    return response.status(201).json();
-  }
+        const orderAll = await knex("order_items")
+        .select([
+            "orders.id",
+            "orders.created_at",
+            "orders.user_id",
+            "orders.status"
+        ]).innerJoin("orders", "orders.id", "order_items.order_id")
+        .where("orders.user_id", user_id).orderBy("created_at")
+        .groupBy("request_id");
 
+        const orderWithItem = await Promise.all(orderAll.map(async order => {
+            const items = await knex("order_items")
+                .select([
+                    "dishes.title as dish_title",
+                    "order_items.amount",
+                    "order_items.order_id"
+                ])
+                .innerJoin("dishes", "dishes.id", "order_items.dish_id")
+                .where("order_items.order_id", order.id);
+        
+            return {
+                ...order,
+                items
+            };
+        }));
+
+        return response.json(orderWithItem);
+    }
+
+    async index(request, response) {
+
+        const orderAll = await knex("order_items")
+        .select([
+            "orders.id",
+            "orders.created_at",
+            "orders.user_id",
+            "orders.status"
+        ]).innerJoin("orders", "orders.id", "order_items.order_id")
+        .orderBy("created_at").groupBy("order_id");
+
+        const orderWithItem = await Promise.all(orderAll.map(async order => {
+            const items = await knex("order_items")
+                .select([
+                    "dishes.name as dish_name",
+                    "order_items.amount",
+                    "order_items.order_id"
+                ])
+                .innerJoin("dishes", "dishes.id", "order_items.dish_id")
+                .where("order_items.order_id", order.id);
+        
+            return {
+                ...order,
+                items
+            };
+        }));
+
+        return response.json(orderWithItem);
+    }
+
+    async update(request, response) {
+        const { id } = request.params;
+        const { status } = request.body;
+        
+        const order = await knex("orders").where({ id }).first();
+
+        if(!order) {
+            throw new AppError("Este prato não existe.", 404)
+        }
+
+        await knex("orders").where({ id }).update({
+            status
+        });
+
+        return response.status(200).json();
+        
+    }
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex("orders").where({ id }).delete();
+
+        return response.json(id);
+    }
 }
 
 module.exports = OrdersController;
--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -1,10 +1,17 @@
 const { Router } = require("express");
-
-
 const OrdersController = require("../controllers/OrdersController");
-const ordersController = new OrdersController();
+
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
 const ordersRoutes = Router();
+const ordersController = new OrdersController();
+
+ordersRoutes.use(ensureAuthenticated);
+
 ordersRoutes.post("/", ordersController.create);
+ordersRoutes.get("/:user_id", ordersController.show);
+ordersRoutes.get("/", ordersController.index);
+ordersRoutes.patch("/:id", ordersController.update);
+ordersRoutes.delete("/:id", ordersController.delete);
 
 module.exports = ordersRoutes;
